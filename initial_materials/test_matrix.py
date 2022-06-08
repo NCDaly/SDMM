1be98fd5b5501c121ef0fd4a7da8b741adffdad5
Nathan Daly <ncdaly@lps.umd.edu>
2022-06-08 15:58:41 -0400

    Added test code

--- initial_materials/test_matrix.py
+++ initial_materials/test_matrix.py
@@ -1,6 +1,76 @@
 from ctypes import *
+import unittest
 
 libmat = CDLL("/home/nathandaly/sdmm-22/initial_materials/matrix.so")
+
+class TestMult(unittest.TestCase):
+    
+    def test_mult1(self):
+        
+        mat1 = [[2, 1, 4],
+                [0, 1, 1]]
+        
+        mat2 = [[6, 3, -1, 0],
+                [1, 1, 0, 4],
+                [-2, 5, 0, 2]]
+        
+        prod = [[5, 27, -2, 12],
+                [-1, 6, 0, 6]]
+        
+        self.assertEqual(pyMult(mat1, mat2), prod)
+        
+    def test_mult2(self):
+
+        mat1 = [[-1, 4],
+                [2, 3]]
+        
+        mat2 = [[9, -3],
+                [6, 1]]
+        
+        prod = [[15, 7],
+                [36, -3]]
+        
+        self.assertEqual(pyMult(mat1, mat2), prod)
+
+    def test_mult3(self):
+        
+        mat1 = [[1, 2, 3],
+                [4, 5, 6]]
+        
+        mat2 = [[10, 11],
+                [20, 21],
+                [30, 31]]
+        
+        prod = [[140, 146],
+                [320, 335]]
+        
+        self.assertEqual(pyMult(mat1, mat2), prod)
+        
+    def test_mult4(self):
+        
+        mat1 = [[3, 4, 2]]
+        
+        mat2 = [[13, 9, 7, 15],
+                [8, 7, 4, 6],
+                [6, 4, 0, 3]]
+        
+        prod = [[83, 63, 37, 75]]
+        
+        self.assertEqual(pyMult(mat1, mat2), prod)
+        
+    def test_mult5(self):
+        
+        mat1 = [[1, 2, 3],
+                [4, 5, 6]]
+        
+        mat2 = [[7],
+                [8],
+                [9]]
+        
+        prod = [[50],
+                [122]]
+        
+        self.assertEqual(pyMult(mat1, mat2), prod)
 
 class MATRIX(Structure):
     
@@ -27,27 +97,13 @@ def unpack(cMat):
             pyMat[i][j] = cMat.contents.data[i * cols + j]
     return pyMat
 
-input_matrix = libmat.input_matrix
-input_matrix.restype = POINTER(MATRIX)
+def pyMult(mat1, mat2):
 
-multiply = libmat.multiply
-multiply.restype = POINTER(MATRIX)
+    return unpack(cMult(pack(mat1), pack(mat2)))
 
-print_matrix = libmat.print_matrix
-print_matrix.restype = None
+cMult = libmat.multiply
+cMult.restype = POINTER(MATRIX)
 
-destroy_matrix = libmat.destroy_matrix
-destroy_matrix.restype = None
+if __name__ == '__main__':
 
-mat1 = pack([[2, 1, 4],
-             [0, 1, 1]])
-mat2 = pack([[6, 3, -1, 0],
-             [1, 1, 0, 4],
-             [-2, 5, 0, 2]])
-prod = multiply(mat1, mat2)
-pyProd = unpack(prod)
-for row in pyProd:
-    print(*row)
-expected = [[5, 27, -2, 12],
-            [-1, 6, 0, 6]]
-print(pyProd == expected)
+    unittest.main()
